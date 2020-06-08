686fc612dedac01c51e1bc90b6bc2fce47ddeff9
Danny Muziga <dannym@bankservafrica.com>
2020-06-08 16:32:11 +0200

    second commit

--- src/main/java/rw/rwandamovie/movieinfoservice/resources/MovieResource.java
+++ src/main/java/rw/rwandamovie/movieinfoservice/resources/MovieResource.java
@@ -2,18 +2,16 @@ package rw.rwandamovie.movieinfoservice.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import rw.rwandamovie.movieinfoservice.models.Movie;
 import rw.rwandamovie.movieinfoservice.models.MovieSummary;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @RestController
 @RequestMapping("/movies")
 public class MovieResource {
-    private static final String template = "Hello Docker File Uploaded, %s!";
-    private final AtomicLong counter = new AtomicLong(100);
 
     @Value("${api.key}")
     private String apiKey;
@@ -29,13 +27,5 @@ public class MovieResource {
 
         return  new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
 
-    }
-
-    @GetMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name",
-            defaultValue="World") String name) {
-
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, name));
     }
 }
